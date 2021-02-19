1f1f6e5570a25f011d82e128fb51d3b5a76e9f26
Jenna Fritz <jennafritz24@gmail.com>
2021-02-19 23:18:25 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even? == true
-    "X"
-  else turn_count(board).odd? == true
-    "O"
-  end
+  turn_count(board).even? == true ? "X" : "O"
 end
