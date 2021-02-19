ceac0b41250599d0f51335ee6134fe24f171f27d
Jenna Fritz <jennafritz24@gmail.com>
2021-02-19 22:50:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,8 @@
 counter = 0
 def turn_count(board)
   board.each do |turn|
-    puts "#{turn}"
-    counter +=1
+    if "{turn}" == "X" || "#{turn}" == "O"
+    counter += 1
   end
-end  
+  counter
+end
