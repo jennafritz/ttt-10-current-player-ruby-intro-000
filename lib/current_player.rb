777cdd467ba8dcd4c23ce4e495ad56290d8da125
Jenna Fritz <jennafritz24@gmail.com>
2021-02-19 22:59:56 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,8 @@
 def turn_count(board)
   counter = 0
-  turns = 0
   board.each do |turn|
     if "{turn}" == "X" || "#{turn}" == "O"
-    turns += 1
-    else turns += 0
+      counter += 1
     end
-    counter += 1
   end
-  turns
 end
